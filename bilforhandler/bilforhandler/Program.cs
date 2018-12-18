539571b9b2687680d8b66ebd88052f4d918caef1
Unknown <nielsenmarcus24@gmail.com>
2018-12-18 14:25:18 +0100

    biler tilføjet
    tilføjet biler og muligheden for at vælge

--- bilforhandler/bilforhandler/Program.cs
+++ bilforhandler/bilforhandler/Program.cs
@@ -8,8 +8,82 @@ namespace bilforhandler
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+
+            Console.WriteLine("Type showall if you want to see the cars");
+            Console.WriteLine("");
+
+            Cars t = new Cars();
+
+
+            bool isRunning = true;
+            while (isRunning == true)
+            {
+                string input = Console.ReadLine();
+
+                string[] inputArray = input.Split(' ');
+
+                switch (inputArray[0].ToLower())
+                {
+                    case "showall":
+                        t.ShowAllCars(t.carModels);
+                        break;
+
+                    case "choose":
+                       t.ChooseCar(t.carModels);
+                       break;
+
+                    case "quit":
+                        isRunning = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("not a valid input");
+                        break;
+                }
+            }
+
         }
+    }
+
+    class Cars
+    {
+        public string[] carModels = new string[] { "Renault Megane", "Kia Ceed", "Mercedes A200", "Tesla Model S 75d" };
+
+        public void ShowAllCars(string[] carModels)
+        {
+            foreach (var car in carModels)
+            {
+                Console.WriteLine(car);
+            }
+        }
+
+
+        public void ChooseCar(string[] carModels)
+        {
+            string userInput = Console.ReadLine();
+
+            if (userInput == "1")
+            {
+                Console.WriteLine("You chose the " + carModels[0]);
+            }
+            else if (userInput == "2")
+            {
+                Console.WriteLine("You chose the " + carModels[1]);
+            }
+            else if (userInput == "3")
+            {
+                Console.WriteLine("You chose the " + carModels[2]);
+            }
+            else if (userInput == "4")
+            {
+                Console.WriteLine("You chose the " + carModels[3]);
+            }
+            else
+            {
+                Console.WriteLine("please write a valid input");
+            }
+        }    
     }
 }
